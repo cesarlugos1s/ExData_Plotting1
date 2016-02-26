84a12c518a57ab3eb50851125c36d9211e0cf26b
Cesar Lugo <cesarlugosis@gmail.com>
2016-02-26 11:41:25 -0600

    Plot 3 added. Plot 1 updated.

--- plot1.R
+++ plot1.R
@@ -1,6 +1,7 @@
 plot1 <- function()
 {
   download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "household_power_comsumption.zip")
+  unzip("household_power_comsumption.zip")
   hpc <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
   hpc$Date <- as.Date(hpc$Date, format = "%d/%m/%Y")
   hpc$Time <- strptime(hpc$Time,format = "%H:%M:%S")
---
+++ plot3.R
@@ -0,0 +1,17 @@
+plot3 <- function()
+{
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "household_power_comsumption.zip")
+  unzip("household_power_comsumption.zip")
+  hpc <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
+  hpc$Time <- strptime(paste(hpc$Date,hpc$Time),format = "%d/%m/%Y%H:%M:%S")
+  hpc$Date <- as.Date(hpc$Date, format = "%d/%m/%Y")
+  hpcfiltered <- subset(hpc, (Date == as.Date("2007-02-01") | Date == as.Date("2007-02-2") ) )
+  hpcfiltered$dayofweek <- as.POSIXlt(hpcfiltered$Date)$wday
+  png(filename="plot3.png")
+  with(hpcfiltered,plot(Time,Sub_metering_1,type = "l",xlab="",ylab = "Energy sub mettering", col = "grey"))
+  lines(hpcfiltered$Time,hpfcfiltered$Sub_metering_2,hpcfiltered$Sub_metering_2, col = "red", type = "l")
+  lines(hpcfiltered$Time,hpfcfiltered$Sub_metering_3,hpcfiltered$Sub_metering_2, col = "blue", type = "l")
+  legend("topright", inset=.05,c("Sub_mettering_1","Sub_mettering_2","Sub_mettering_3"), fill = c("gray","red","blue"))
+  dev.off()
+
+}
